6c3e5759a9d4d0001a570504960a1449185552e5
Aleksandr Loschev <alex@loschev.com>
2020-03-15 18:29:24 +0300

    Create data base, fill it

---
+++ webapp/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+from webapp.model import db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    # @app.route("/")
+    # def index():
+    #     title = "Drumtools"
+    #     return render_template('index.html', page_title=title)
+
+    return app
