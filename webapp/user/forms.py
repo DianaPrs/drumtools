82ab177246ccde4c57f8f43b8c9571cb151c7393
deepsee <freed@li.ru>
2020-04-02 19:49:27 +0300

    Add Navbar

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField, HiddenField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from webapp.user.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -13,5 +15,15 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat assword', validators=[DataRequired(), EqualTo('password')])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up', render_kw={"class": "btn btn-primary"})
+
+    def validate_username(self, username):
+        users_count = User.query.filter_by(username=username.data).count()
+        if users_count > 0:
+            raise ValidationError('Username already exists')
+
+    def validate_email(self, email):
+        users_count = User.query.filter_by(email=email.data).count()
+        if users_count > 0:
+            raise ValidationError('An account with that email already exists')
